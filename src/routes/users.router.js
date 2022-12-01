b5afec2339778dca2adc25a630e09f3dfa5fac59
gabriela Arenas <gabega10@gmail.com>
2022-12-01 20:21:32 -0300

    terminado

---
+++ src/routes/users.router.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import userController from '../controllers/users.controller.js'
+
+
+
+
+const router = Router();
+
+
+router.get('/',userController.getUsers)
+
+
+
+
+
+export default router
